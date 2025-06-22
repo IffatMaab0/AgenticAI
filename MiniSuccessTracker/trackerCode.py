3c46bb58119aecae4a229a726a387194ff974b20
IffatMaab0 <iffatmaab11@gmail.com>
2025-06-22 20:24:36 +0500

    details added

--- MiniSuccessTracker/trackerCode.py
+++ MiniSuccessTracker/trackerCode.py
@@ -1,39 +1,43 @@
+## Author : Iffat Maab
+## project : Coding journey tracking
+## Date : 21-06-2025
+
 import streamlit as st
 from datetime import date
 import pandas as pd
 import os
 
 
-## INTRO
+
 st.title("Coding Journey")
 st.text(" Logic will get you from A to Z; imagination will get you everywhere.")
 
-##Import date
+
 today = date.today()
 
-## taking input from user
+
 task = st.text_input("Enter Task")
 understanding = st.text_area("What i understood")
 confused = st.text_area("What confused me")
 
-# File name where diary entries will be stored
+
 file = "Coding_Diary.csv"
 
 if st.button("Save Entry"):
-    # If all fields are empty, show a warning
+    
     if task.strip() == "" and understanding.strip() == "" and confused.strip() == "":
         st.warning("Write something, even intentions matter. So write what you decided to do but couldn't")
     else:
-         # Create a new entry as a DataFrame
+        
         entry = pd.DataFrame([{"Date": today,"Task Tried": task, "Understanding": understanding,"Confusion": confused}])
 
-     # If file already exists, read old data and add new entry to it
+    
     if os.path.exists(file):
         preData= pd.read_csv(file) 
         df = pd.concat([preData, entry], ignore_index= True)
 
     else:   
-        # If file doesn't exist, this is the first entry    
+          
         df = entry
 
     # Save updated data to CSV file    
